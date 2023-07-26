9b9a5b62cf414cb6af962f35544f288800f1d954
Kirat Bains <kiratbains09@gmail.com>
2023-07-25 23:50:00 -0700

    feat: early setup, library and package installation

---
+++ scrapper.py
@@ -0,0 +1,11 @@
+import requests
+from bs4 import BeautifulSoup
+
+URL = 'https://www.amazon.ca/GLASHOM-Standing-Hanging-Leaning-Arched-Top/dp/B0BYK7S6PM/ref=sr_1_5?keywords=wall%2Bmirror%2Bfull%2Blength&qid=1690353934&sprefix=wall%2Bmirror%2Caps%2C242&sr=8-5&ufe=app_do%3Aamzn1.fos.d0e27fc4-6417-4b26-97cb-f959a9930752&th=1'
+
+headers = {"User-Agent": 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.3 Safari/605.1.15'}
+
+page = requests.get(URL, headers=headers)
+
+soup = BeautifulSoup(page.content, 'html.parser')
+
