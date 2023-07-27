79184ecd2e572b15ca441eb10ae76b569e6e0038
Kirat Bains <kiratbains09@gmail.com>
2023-07-27 00:11:43 -0700

    feat: webpage code

--- scrapper.py
+++ scrapper.py
@@ -15,3 +15,7 @@ converted_price = float(price[5:]) #the first 5 characters are the currency symb
 
 print(converted_price) 
 print(title.strip())
+
+
+#price section of webpage, through inspect element. 
+<div class="a-section a-spacing-none aok-align-center">                <span class="a-price aok-align-center reinventPricePriceToPayMargin priceToPay" data-a-size="xl" data-a-color="base"><span class="a-offscreen">$179.99</span><span aria-hidden="true"><span class="a-price-symbol">$</span><span class="a-price-whole">179<span class="a-price-decimal">.</span></span><span class="a-price-fraction">99</span></span></span>              <span id="taxInclusiveMessage" class="a-size-mini a-color-base aok-align-center aok-nowrap">  </span>          </div>
